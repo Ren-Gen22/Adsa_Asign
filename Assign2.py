b3264531410658f341f67e497f6c0bb43fb02ddc
Ren-Gen22 <abhishekrenjan33@gmail.com>
2023-09-30 23:10:39 +0530

    Slight addition

--- Assign2.py
+++ Assign2.py
@@ -31,3 +31,50 @@ print("Enter the matrix dimensions in a single line:\t\tlike 10 30 5 60 20")
 matrix_dimensions = list(map(int,input().split()))
 min_scalar_multiplications = matrix_chain_multiplication(matrix_dimensions)
 print(f'Minimum scalar multiplications: {min_scalar_multiplications}')
+print("""
+Initialization:
+
+We create two matrices, M and S, each of size NxN, where N is the number of matrices in the sequence.
+M[i][j] will store the minimum number of scalar multiplications needed to compute the product of matrices 
+      Ai to Aj.
+S[i][j] will store the value of k that minimizes M[i][j], representing the optimal split point.
+Recurrence Relation:
+
+We fill in the M matrix using a bottom-up approach, starting with subchains of length 2 and gradually 
+      building up to the full chain length.
+
+The general recurrence relation for filling in M[i][j] is:
+
+M[i][j] = min(M[i][k] + M[k+1][j] + p[i-1] * p[k] * p[j]) for all i ≤ k < j
+This formula represents the cost of parenthesizing the matrices between i and j at position k and 
+      computing the cost of multiplying these parenthesized subproblems.
+
+Reconstruction of Optimal Parenthesization:
+
+To reconstruct the optimal way to parenthesize the matrices, we create a function that takes two indices, 
+      i and j, and recursively determines the optimal split point using the S matrix.
+We split the matrices at position S[i][j] and recursively call the function for the left and right 
+      subchains until we reach base cases.
+This process helps us build the optimal parenthesization step by step.
+
+      
+Time and Space Complexity Analysis:
+
+Time Complexity:
+
+Filling in the M matrix requires O(N^3) operations since there are three nested loops: one for the chain 
+      length and two for the subchain indices (i and j).
+Reconstructing the optimal parenthesization also takes O(N^3) time in the worst case.
+Space Complexity:
+
+We use two matrices, M and S, each of size NxN, so the space complexity is O(N^2) for storing these
+       matrices.
+Efficiency for Large Instances:
+
+The dynamic programming approach is efficient for moderately large instances of the problem where N is 
+      not too large.
+However, it becomes impractical for very large N values due to its cubic time complexity. In such cases, 
+      more advanced algorithms like Strassen's algorithm for matrix multiplication or distributed computing approaches may be used.
+For most practical applications, the dynamic programming approach works well and provides the optimal 
+      solution in a reasonable amount of time.
+""")
--- Assign3.py
+++ Assign3.py
@@ -41,3 +41,37 @@ for solution in solutions:
     for row in solution:
         print(" ".join(row))
     print()
+print("""
+Backtracking Approach for N-Queens:
+
+The backtracking approach for solving the N-Queens problem involves trying out different possibilities 
+      and, when a conflict is encountered, backtracking to explore alternative choices. Here's how it
+       works:
+
+- Solution Generation: The goal is to place N queens on an NXN chessboard in such a way that no two queens
+       threaten each other. The algorithm generates solutions incrementally by placing queens one by one in
+       different rows while ensuring that they do not conflict with each other.
+
+- Validation (is_safe function): Before placing a queen in a particular cell, we need to validate whether
+       it's safe to do so. The `is_safe` function checks three conditions:
+  1. No queen in the same column as the current cell.
+  2. No queen in the upper-left diagonal.
+  3. No queen in the upper-right diagonal.
+
+- Conflict Resolution: If it's safe to place a queen in a cell, we mark that cell as 'Q' and move on to 
+      the next row. If at any point we find that it's not possible to place a queen in any cell of the 
+      current row without violating the safety conditions, we backtrack. Backtracking means going back to 
+      the previous row and exploring a different column to place the queen.
+
+Time Complexity and Performance:
+
+- In the worst case, where all possible solutions are generated, the time complexity is exponential, 
+      approximately O(N!).
+- On average, the backtracking algorithm prunes many possibilities early on, reducing the search space. 
+      In such cases, the time complexity can be closer to O(N^N).
+
+The performance for larger N values deteriorates rapidly due to the exponential nature of the problem. 
+      For small N (e.g., N = 8 or N = 10), the algorithm can find solutions relatively quickly. However,
+       as N increases, the time it takes to find solutions grows significantly.
+
+""")
